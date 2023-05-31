b3f62817fbe28cff82f597a0599fe47bb26e67dc
Yannick.Nteham <tinyannickz@gmail.com>
2023-05-31 18:16:55 -0400

    subnet changed

--- main.tf
+++ main.tf
@@ -14,24 +14,24 @@ resource "aws_internet_gateway" "main" {
 }
 
 resource "aws_subnet" "public1" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.0.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
   tags = {
     Name = "yannick-publicsubnet"
-    env = "Dev"
+    env  = "Dev"
   }
 }
 
-resource "aws_subnet" "public1" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1b"
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
   tags = {
     Name = "yannick-publicsubnet"
-    env = "Dev"
+    env  = "Dev"
   }
 }
 
@@ -39,17 +39,22 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   tags = {
     Name = "yannick-publicroutetable"
-    env = "Dev"
+    env  = "Dev"
   }
 }
 
 resource "aws_route" "public" {
-  route_table_id = aws_route_table.public.id
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.main.id
+  gateway_id             = aws_internet_gateway.main.id
 }
 
-resource "aws_route_table_association" "public" {
+resource "aws_route_table_association" "public1" {
   route_table_id = aws_route_table.public.id
-  subnet_id = [aws_subnet.public1.id, aws_subnet.public2.id]
+  subnet_id      = aws_subnet.public1.id
+}
+
+resource "aws_route_table_association" "public2" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public2.id
 }
