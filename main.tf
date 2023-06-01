910ddc05ca53df9cc4c5590c26487a1f83cd9023
Yannick.Nteham <tinyannickz@gmail.com>
2023-06-01 11:40:46 -0400

    added variables

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  instance_tenancy     = "default"
+  cidr_block           = var.vpc_cidr
+  instance_tenancy     = var.instance_tenancy
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
@@ -13,10 +13,20 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_subnet" "public1" {
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.main.id
+  subnet_id     = aws_subnet.public[0].id
+}
+
+resource "aws_eip" "main" {
+
+}
+
+resource "aws_subnet" "public" {
+  count                   = var.public_subnet_count
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-1a"
+  cidr_block              = cidrsubnet(var.vpc_cidr, var.subnet_bits, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
   map_public_ip_on_launch = true
   tags = {
     Name = "yannick-publicsubnet"
@@ -24,13 +34,13 @@ resource "aws_subnet" "public1" {
   }
 }
 
-resource "aws_subnet" "public2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
+resource "aws_subnet" "private" {
+  count             = var.private_subnet_count
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, var.subnet_bits, count.index + var.public_subnet_count)
+  availability_zone = element(var.availability_zones, count.index)
   tags = {
-    Name = "yannick-publicsubnet"
+    Name = "yannick-privatesubnet"
     env  = "Dev"
   }
 }
@@ -49,12 +59,28 @@ resource "aws_route" "public" {
   gateway_id             = aws_internet_gateway.main.id
 }
 
-resource "aws_route_table_association" "public1" {
+resource "aws_route_table_association" "public" {
+  count          = var.public_subnet_count
   route_table_id = aws_route_table.public.id
-  subnet_id      = aws_subnet.public1.id
+  subnet_id      = aws_subnet.public[count.index].id
 }
 
-resource "aws_route_table_association" "public2" {
-  route_table_id = aws_route_table.public.id
-  subnet_id      = aws_subnet.public2.id
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "yannick-privateroutetable"
+    env  = "Dev"
+  }
+}
+
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.main.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = var.private_subnet_count
+  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.private[count.index].id
 }
